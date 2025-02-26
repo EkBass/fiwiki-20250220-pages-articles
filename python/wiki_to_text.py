273391ac9c29a17060e9420ca24c537a571516af
Kristian Virtanen <krisu.virtanen@gmail.com>
2025-02-26 19:32:53 +0200

    Update wiki_to_text.py

--- python/wiki_to_text.py
+++ python/wiki_to_text.py
@@ -1,8 +1,9 @@
 from dewiki_functions import *
 
-#wiki_xml_file = 'F:/simplewiki-20210401/simplewiki-20210401.xml'  # update this
 wiki_xml_file = 'xml.xml'  # update this
 json_save_dir = './text/'
+wiki_xml_path = "../"
+daFile = wiki_xml_path + wiki_xml_file
 
 if __name__ == '__main__':
     process_file_text(wiki_xml_file)
